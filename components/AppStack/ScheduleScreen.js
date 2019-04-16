03840d2c875f24587293bc66a3de897ad0c5a870
beaagron <baagron@gmail.com>
2019-04-15 23:33:39 -0500

    todo list

--- components/AppStack/ScheduleScreen.js
+++ components/AppStack/ScheduleScreen.js
@@ -4,18 +4,18 @@ import {
     View,
     StyleSheet,
     ScrollView,
-    TextInput,
-    TouchableOpacity,
     SafeAreaView,
     Image,
-    StatusBar,
-    SectionList
+    ActivityIndicator,
+    AsyncStorage
 } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
-import CalendarStrip from 'react-native-calendar-strip';
+import uuid from 'uuid/v1';
 import moment from 'moment';
+
 import TaskCard from '../TaskCard';
 import FrontCard from '../FrontCard';
+import TaskInput from '../TaskInput';
 
 export default class ScheduleScreen extends Component {
 
@@ -28,15 +28,115 @@ export default class ScheduleScreen extends Component {
         this.state = {
             currentDate: new Date(),
             markedDate: moment(new Date()).format("YYYY-MM-DD"),
-            // advDays: 0
         };
     }
 
-    // NextDay(numDays){
-    //     this.setState({
-    //         advDays: moment(this.state.currentDate).add(numDays, 'days').format("MMMM D")
-    //     })
-    // }
+    state = {
+        inputValue: '',
+        loadingItems: false,
+        allItems: {},
+        isCompleted: false
+    };
+
+    componentDidMount = () => {
+        this.loadingItems();
+    };
+
+    newInputValue = value => {
+        this.setState({
+            inputValue: value
+        });
+    };
+
+    loadingItems = async () => {
+        try {
+            const allItems = await AsyncStorage.getItem('Todos');
+            this.setState({
+                loadingItems: true,
+                allItems: JSON.parse(allItems) || {}
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    onDoneAddItem = () => {
+        const { inputValue } = this.state;
+        if (inputValue !== '') {
+            this.setState(prevState => {
+                const id = uuid();
+                const newItemObject = {
+                    [id]: {
+                        id,
+                        isCompleted: false,
+                        text: inputValue,
+                        createdAt: Date.now()
+                    }
+                };
+                const newState = {
+                    ...prevState,
+                    inputValue: '',
+                    allItems: {
+                        ...prevState.allItems,
+                        ...newItemObject
+                    }
+                };
+                this.saveItems(newState.allItems);
+                return { ...newState };
+            });
+        }
+    };
+
+    deleteItem = id => {
+        this.setState(prevState => {
+            const allItems = prevState.allItems;
+            delete allItems[id];
+            const newState = {
+                ...prevState,
+                ...allItems
+            };
+            this.saveItems(newState.allItems);
+            return { ...newState };
+        });
+    };
+
+    completeItem = id => {
+        this.setState(prevState => {
+            const newState = {
+                ...prevState,
+                allItems: {
+                    ...prevState.allItems,
+                    [id]: {
+                        ...prevState.allItems[id],
+                        isCompleted: true
+                    }
+                }
+            };
+            this.saveItems(newState.allItems);
+            return { ...newState };
+        });
+    };
+
+    incompleteItem = id => {
+        this.setState(prevState => {
+            const newState = {
+                ...prevState,
+                allItems: {
+                    ...prevState.allItems,
+                    [id]: {
+                        ...prevState.allItems[id],
+                        isCompleted: false
+                    }
+                }
+            };
+            this.saveItems(newState.allItems);
+            return { ...newState };
+        });
+    };
+
+    saveItems = newItem => {
+        const saveItem = AsyncStorage.setItem('Todos', JSON.stringify(newItem));
+    };
 
     render() {
 
@@ -44,10 +144,11 @@ export default class ScheduleScreen extends Component {
         const today = this.state.currentDate;
         const day = moment(today).format("dddd");
         const date = moment(today).format("MMMM D");
+        const { inputValue, loadingItems, allItems } = this.state;
 
         return (
             <SafeAreaView style={{ flex: 1 }}>
-                <View style={styles.container2}>
+                {/* <View style={styles.container2}>
                     <View style={styles.titleBar}>
                         <Image
                             style={styles.avatar}
@@ -71,68 +172,46 @@ export default class ScheduleScreen extends Component {
                             destination="Detail"
                         />
                     </View>
-                </View>
+                </View> */}
                 <View
                     style={styles.container}
                 >
-                    <View style={{ flexDirection: "row" }}>
-                        <View style={styles.searchContainer}>
-                            <TextInput
-                                style={styles.searchBar}
-                                placeholder="Quick Search"
-                                placeholderTextColor='grey'
-                                autoCorrect={false}
-
-                            />
-                        </View>
-                        <TouchableOpacity
-                            style={styles.newButtonContainer}
-                            onPress={() => navigate('NewTask')}
-                        >
-                            <Ionicons name="ios-add" size={40} color="grey" />
-                        </TouchableOpacity>
-                    </View>
                     <View style={styles.headerContainer}>
                         <Text style={styles.headerText}>Your To Do List:</Text>
                     </View>
+                    <View style={{ flexDirection: "row" }}>
+                        <View style={styles.searchContainer}>
+                            <TaskInput
+                                inputValue={inputValue}
+                                onChangeText={this.newInputValue}
+                                onDoneAddItem={this.onDoneAddItem}
+                            />
+                        </View>
+                    </View>
                     <View style={styles.container}>
-                        <CalendarStrip
-                            style={styles.calendar}
-                            showMonth={false}
-                            calendarAnimation={{ type: 'sequence', duration: 30 }}
-                            daySelectionAnimation={{ type: 'border', duration: 300, borderWidth: 1, borderHighlighterColor: 'tomato' }}
-                            dateNameStyle={{ color: '#979797', fontFamily: 'Avenir Next' }}
-                            dateNumberStyle={{ color: '#979797', fontFamily: 'Avenir Next', fontWeight: '400' }}
-                        />
-
-                        <View style={styles.tasks}>
-                            <ScrollView
-                                showsVerticalScrollIndicator={false}
-                            >
-                                <Text style={styles.date}>Today · {date}</Text>
-                                <TaskCard
-                                    taskName="Process 2336 Notes"
-                                    classColor="#E8384F"
-                                />
-                                <TaskCard
-                                    taskName="Read Chapter 13"
-                                    classColor="#4186E0"
-                                />
-                                <Text style={styles.date}>Tomorrow · {moment(today).add(1, 'days').format("MMMM D")}</Text>
-                                <TaskCard
-                                    taskName="Finish HW 25"
-                                    classColor="#FD612C"
-                                />
-                                <TaskCard
-                                    taskName="Finish Math OHW"
-                                    classColor="#62D26F"
-                                />
-                                <Text style={styles.date}>{moment(today).add(2, 'days').format("dddd")}, {moment(today).add(2, 'days').format("MMMM D")}</Text>
-                                <TaskCard
-                                    taskName="Finish HW 17"
-                                    classColor="#A4CF30"
-                                />
-                            </ScrollView>
+                        <View style={styles.list}>
+                            <View style={styles.column}>
+                                <Text style={styles.name}>RECENT TASKS</Text>
+                            </View>
+                            {loadingItems ? (
+                                <ScrollView
+                                    contentContainerStyle={styles.scrollableList}
+                                >
+                                    {Object.values(allItems)
+                                        .reverse()
+                                        .map(item => (
+                                            <TaskCard
+                                                key={item.id}
+                                                {...item}
+                                                deleteItem={this.deleteItem}
+                                                completeItem={this.completeItem}
+                                                incompleteItem={this.incompleteItem}
+                                            />
+                                        ))}
+                                </ScrollView>
+                            ) : (
+                                    <ActivityIndicator size="large" color="white" />
+                                )}
                         </View>
                     </View>
                 </View>
@@ -144,22 +223,17 @@ export default class ScheduleScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 10
     },
     searchContainer: {
         flex: 4,
         paddingVertical: 20,
         alignContent: 'center'
     },
-    newButtonContainer: {
-        paddingLeft: 23,
-        flex: .5,
-        marginTop: 20
-    },
     headerContainer: {
-        paddingLeft: 10,
+        paddingLeft: 13,
         justifyContent: 'center',
-        alignContent: 'center'
+        alignContent: 'center',
+        marginTop: 40
     },
     headerText: {
         fontFamily: 'Avenir Next',
@@ -168,13 +242,8 @@ const styles = StyleSheet.create({
     },
     searchBar: {
         height: 40,
-        backgroundColor: '#F2F2F2',
         color: 'black',
-        paddingHorizontal: 20,
-        borderWidth: 1,
-        borderColor: '#F2F2F2',
-        borderRadius: 5,
-        fontFamily: 'Avenir Next'
+        paddingHorizontal: 10,
     },
     calendar: {
         paddingTop: 5,
@@ -183,7 +252,6 @@ const styles = StyleSheet.create({
         height: 75
     },
     date: {
-        marginTop: 20,
         marginLeft: 10,
         fontFamily: 'Avenir Next',
         fontSize: 20,
@@ -220,13 +288,35 @@ const styles = StyleSheet.create({
         fontWeight: '500',
     },
     name: {
-        fontSize: 20,
+        fontSize: 15,
         fontFamily: 'Avenir Next',
-        color: '#3c4560',
-        fontWeight: '700',
-        marginTop: -3,
+        color: '#b8bece',
+        fontWeight: '600',
     },
     contentBar: {
         alignItems: 'center'
+    },
+    centered: {
+        alignItems: 'center'
+    },
+    inputContainer: {
+        marginTop: 40,
+        paddingLeft: 15
+    },
+    list: {
+        flex: 1,
+        paddingLeft: 15,
+        marginBottom: 10
+    },
+    scrollableList: {
+        marginTop: 15
+    },
+    column: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    deleteAllButton: {
+        marginRight: 40
     }
 })
--- components/TaskCard.js
+++ components/TaskCard.js
@@ -1,80 +1,139 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
-    Text,
     View,
+    Text,
+    Dimensions,
+    StyleSheet,
     TouchableOpacity,
+    Platform
 } from 'react-native';
-import { withNavigation } from 'react-navigation';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+const { height, width } = Dimensions.get('window');
+import { MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
 import TaskButton from './TaskButton'
 
 class TaskCard extends Component {
 
+    onToggleCircle = () => {
+        const { isCompleted, id, completeItem, incompleteItem } = this.props;
+        if (isCompleted) {
+            incompleteItem(id);
+        } 
+        else {
+            completeItem(id);
+        }
+    };
+
     render() {
-        const { taskName, classColor } = this.props
+        const { text, deleteItem, id, isCompleted, classColor } = this.props;
         return (
             <View style={styles.container}>
-                <View style={styles.radioButtonContainer}>
-                    <TaskButton
-                        classColor={classColor}
-                    />
-                    <View style={styles.headerContainer}>
-                        <Text style={styles.headerText}>
-                            {taskName}
-                        </Text>
-                    </View>
-                </View>
-                <View style={styles.editContainer}>
-                    <TouchableOpacity
-                        onPress={() => {
-                            this.props.navigation.navigate('TaskDetail', {
-                                taskName,
-                                classColor
-                            })
-                        }}
-                    >
-                        <MaterialCommunityIcons name="dots-horizontal" size={30} color="black" />
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this.onToggleCircle}>
+                        <View
+                            style={[
+                                styles.circle,
+                                isCompleted
+                                    ? { borderColor: 'green' }
+                                    : { borderColor: 'tomato' }
+                            ]}
+                        />
                     </TouchableOpacity>
+                    <Text
+                        style={[
+                            styles.text,
+                            isCompleted
+                                ? {
+                                    color: 'grey',
+                                    textDecorationLine: 'line-through'
+                                }
+                                : { color: 'black' }
+                        ]}
+                    >
+                        {text}
+                    </Text>
                 </View>
+                {isCompleted ? (
+                    <View style={styles.button}>
+                        <TouchableOpacity onPressOut={() => deleteItem(id)}>
+                            <MaterialIcons
+                                name="delete-forever"
+                                size={24}
+                                color='grey'
+                            />
+                        </TouchableOpacity>
+                    </View>
+                ) : null}
             </View>
         )
     }
 }
 
-export default withNavigation(TaskCard);
+export default TaskCard;
 
 const styles = StyleSheet.create({
+    // container: {
+    //     padding: 15,
+    //     backgroundColor: '#fff',
+    //     flexDirection: 'row',
+    //     alignItems: 'center'
+    // },
+    // headerContainer: {
+    //     flex: 6,
+    //     flexDirection: 'row',
+    //     //backgroundColor: 'yellow',
+    // },
+    // editContainer: {
+    //     width: 30,
+    //     flexDirection: 'row',
+    //     marginLeft: 10,
+    //     justifyContent: 'flex-end',
+    //     //backgroundColor: 'orange'
+    // },
+    // headerText: {
+    //     fontFamily: 'Avenir Next',
+    //     fontSize: 17,
+    //     fontWeight: '600',
+    //     marginLeft: 7.5
+    // },
+    // radioButtonContainer: {
+    //     flex: 1,
+    //     flexDirection: 'row',
+    //     justifyContent: 'flex-start',
+    //     alignItems: 'center',
+    //     marginLeft: 10,
+    //     //backgroundColor: 'green'
+    // }
     container: {
-        padding: 15,
-        backgroundColor: '#fff',
+        width: width - 50,
         flexDirection: 'row',
-        alignItems: 'center'
-    },
-    headerContainer: {
-        flex: 6,
-        flexDirection: 'row',
-        //backgroundColor: 'yellow',
-    },
-    editContainer: {
-        width: 30,
-        flexDirection: 'row',
-        marginLeft: 10,
-        justifyContent: 'flex-end',
-        //backgroundColor: 'orange'
-    },
-    headerText: {
-        fontFamily: 'Avenir Next',
-        fontSize: 17,
-        fontWeight: '600',
-        marginLeft: 7.5
-    },
-    radioButtonContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
+        borderBottomWidth: 1,
+        borderRadius: 5,
+        borderColor: 'grey',
+        backgroundColor: 'white',
+        height: width / 8 + 4,
         alignItems: 'center',
-        marginLeft: 10,
-        //backgroundColor: 'green'
-    }
+        justifyContent: 'space-between',
+        marginVertical: 5,
+      },
+      column: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: width / 1.5
+      },
+      text: {
+        fontWeight: '500',
+        fontSize: 16,
+        marginVertical: 15,
+        fontFamily: 'Avenir Next',
+      },
+      circle: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderWidth: 3,
+        margin: 10
+      },
+      button: {
+        marginRight: 10
+      }
 })
